6edd81663bb528e5078de933947d47e43177fcca
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-09-06 20:37:37 -0400

    post comments route  added

--- data/comments.mjs
+++ data/comments.mjs
@@ -1,8 +1,8 @@
 export const comments = [
     {
+    id: 1,
     userId: 1,
     postId:1,
-    commentId:1,
-    commentText:"excellent work"
+    body:"excellent work"
 }
 ];
--- routes/commentRoutes.mjs
+++ routes/commentRoutes.mjs
@@ -7,7 +7,25 @@ router
 .route("/")
 .get((req, res) => {
     res.json(comments)
-});
+})
+.post((req, res) => {
+    // Get the content (userId, postId and body from the request)
+        const { userId, postId, body} = req.body;
+        let id = comments[comments.length - 1].id + 1;
+    // create a post by initializing an id
+    if (userId && postId && body) {
+        const comment = {
+          id: id,
+          userId: userId,
+          postId: postId,
+          body: body,
+        };
+        comments.push(comment);
+        res.json(comments[comments.length - 1]);
+      } else {
+        res.json({ error: "Insufficient Data" });
+      }
+    })
 
 
 
